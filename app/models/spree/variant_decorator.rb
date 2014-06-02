24a0d54a77898ea8af98c681f26a4fe92b1f4e17
Nima Shariatian <nima@onevio.org>
2014-06-02 10:31:41 -0400

    bugfix: made sure count_on_hand_was is defined

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,11 +5,14 @@ Spree::Variant.class_eval do
 
 private
   def notify_availability
-    available = count_on_hand_was <= 0 && count_on_hand > 0 && count_on_hand_changed?
+
+    if (defined? count_on_hand_was) && (defined? count_on_hand) && (defined? count_on_hand_changed?)
+      available = count_on_hand_was <= 0 && count_on_hand > 0 && count_on_hand_changed?
+    end
 
     yield
     
-    if available
+    if (defined? available) && (available)
       stock_requests.notified(false).each &:notify!
       product.stock_requests.without_variant.notified(false).each &:notify!
     end
