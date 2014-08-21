bf2ef30612e842efc6a711ee9f7ff2bc35b3e4c2
Nima Shariatian <nima@onevio.org>
2014-08-21 13:21:31 -0400

    updates: routes to use spree 2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.add_routes do
   resources :stock_requests
   namespace :admin do
     resources :reports, :only => [] do
       collection do
-        get  :stock_requests
+        get :stock_requests
         post :stock_requests
       end
     end
